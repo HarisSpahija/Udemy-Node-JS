f5bfeb29ce242df532cd0559cec78637a75967df
HarisSpahija <haris.spahija@hybrit.org>
2019-04-15 14:01:29 +0200

    Added an extra create user page

--- section-3/assignment/routes.js
+++ section-3/assignment/routes.js
@@ -1,61 +1,80 @@
 const requestHandler = (req, res) => {
   const { url } = req;
   const users = ["Paul", "Frank", "Foo", "Clair", "Joe"];
-  if (url === '/') {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Greeting message</title></head>');
-    res.write('<body><h1>Home page</h1></body>')
-    res.write('</html>');
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Assignment 1</title></head>");
+    res.write("<body>");
+    res.write(
+      '<form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></form>'
+    );
+    res.write("</body>");
+    res.write("</html>");
     return res.end();
   }
-  if (url === '/users') {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Greeting message</title></head>');
-    res.write('<body>');
-    res.write('<h1>Users</h1>')
+  if (url === "/users") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Greeting message</title></head>");
+    res.write("<body>");
+    res.write("<h1>Users</h1>");
     res.write("<ul>");
     users.map(user => {
       res.write(`<li>${user}</li>`);
     });
     res.write("</ul>");
-    res.write('</body>');
-    res.write('</html>');
+    res.write("</body>");
+    res.write("</html>");
     return res.end();
   }
+  if (url === "/create-user") {
+    const body = [];
+    req.on("data", chunk => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const newUsername = parsedBody.split('=')[1];
+      console.log(newUsername);
+      users.push(newUsername);
+    });
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    res.end();
+  }
 
-    // Root route
-//     res.setHeader("Content-Type", "text/html");
-//     res.write("<head><title>Greeting message</title></head>");
-//     res.write('<body><h1>Here is some greeting message</h1><form action="/create-user" method="POST"><input type="text" name="createdUser"><button type="submit">Create User</button></form></body>'); 
-//   }
-//   // Users Route
-//   if (url === "/users") {
-//     res.write("<ul>");
-//     users.map(user => {
-//       res.write(`<li>${user}</li>`);
-//     });
-//     res.write("</ul>");
-//   }
+  // Root route
+  //     res.setHeader("Content-Type", "text/html");
+  //     res.write("<head><title>Greeting message</title></head>");
+  //     res.write('<body><h1>Here is some greeting message</h1><form action="/create-user" method="POST"><input type="text" name="createdUser"><button type="submit">Create User</button></form></body>');
+  //   }
+  //   // Users Route
+  //   if (url === "/users") {
+  //     res.write("<ul>");
+  //     users.map(user => {
+  //       res.write(`<li>${user}</li>`);
+  //     });
+  //     res.write("</ul>");
+  //   }
 
-//   if (url === "/create-user") {
-//     const body = []
-//     req.on("data", chunk => {
-//         body.push(chunk);
-//     });
-//     return req.on("end", () => {
-//         const parsedBody = Buffer.concat(body).toString();
-//         const newUser = parsedBody.split("=")[1];
-//         console.log(newUser)
-//       });
-// }
-//   res.setHeader("Content-Type", "text/html");
-//   res.write("<html>");
-//   res.write("<head><title>My First assignment</title></head>");
-//   res.write("<body><h1>Hello from my first assignment!</h1></body>");
-//   res.write("</html>");
-//   res.end();
+  //   if (url === "/create-user") {
+  //     const body = []
+  //     req.on("data", chunk => {
+  //         body.push(chunk);
+  //     });
+  //     return req.on("end", () => {
+  //         const parsedBody = Buffer.concat(body).toString();
+  //         const newUser = parsedBody.split("=")[1];
+  //         console.log(newUser)
+  //       });
+  // }
+  //   res.setHeader("Content-Type", "text/html");
+  //   res.write("<html>");
+  //   res.write("<head><title>My First assignment</title></head>");
+  //   res.write("<body><h1>Hello from my first assignment!</h1></body>");
+  //   res.write("</html>");
+  //   res.end();
 };
 
 exports.handler = requestHandler;
