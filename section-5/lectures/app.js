be504dcec4781306c1d6a6d21e614013c01f4563
HarisSpahija <harisspahija@gmail.com>
2019-04-17 17:30:04 +0200

    lecture 67

--- section-5/lectures/app.js
+++ section-5/lectures/app.js
@@ -1,19 +1,18 @@
-const http = require("http");
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use('/add-product', (req, res, next) => {
-    console.log('In another middleware!')
-    res.send(`<h1>Hello from Product Page!</h1>`);
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+
 });
-
-app.use('/', (req, res, next) => {
-    console.log('In another middleware!')
-    res.send(`<h1>Hello from Express!</h1>`);
-    // ... this is the next middleware in line
-});
-
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
