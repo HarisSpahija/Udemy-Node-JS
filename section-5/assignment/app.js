bae8194b9058be6060c2798ea83d35a4f4c79145
HarisSpahija <harisspahija@gmail.com>
2019-04-17 16:41:16 +0200

    Made assignment from section 5

---
+++ section-5/assignment/app.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const express = require("express");
+
+const app = express()
+
+app.use('/', (req, res, next) => {
+    console.log('First root route')
+    next();
+});
+
+app.use('/users', (req, res, next) => {
+    console.log('User route');
+    res.send(`<h1>Hello from the user page</h1>`)
+});
+
+app.use('/', (req, res, next) => {
+    console.log('Main Root route');
+    res.send(`<h1>Hello from the home page</h1>`);
+});
+
+const server = http.createServer(app);
+
+server.listen(3000);
